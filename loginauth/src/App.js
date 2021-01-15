e8727e15b9cdf5a53d37050aa9ea2f120f686bc5
Mahesh <Mahesh.lkp1234@gmail.com>
2021-01-15 05:06:40 -0600

    added auth changes

---
+++ loginauth/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React from 'react';
+// import { useAuth0 } from '@auth0/auth0-react';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile'
+import { useAuth0 } from '@auth0/auth0-react';
+
+function App () {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading..</div>
+  return (
+    <div className="App">
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
